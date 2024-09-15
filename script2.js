da31ed8656209436c11ce5661d47300148303087
Dima <belyaev-markov@yandex.ru>
2024-09-15 05:04:55 +0300

    commit 1

--- script2.js
+++ script2.js
@@ -1,8 +1,7 @@
+
 function print(param, clas) {
   var cell = document.createElement("p");
-  /*cell.classList.add("grid__cell");
-  cell.classList.add(clas);*/
-  cell.className = "grid__cell clas"
+  cell.classList.add("grid__cell", clas);
   cell.innerHTML = param;
   document.querySelector(".grid").appendChild(cell);
 }
@@ -12,25 +11,46 @@ function ciclies(par1, par2) {
   for (let index = 0; index < 80; index++) {
     arr[0] = par1;
     arr[1] = par2;
-    arr[(index + 2)] = arr[index] + (arr[(index + 1)] * 1);
+    arr[(index + 2)] = arr[index] + arr[(index + 1)];
     print(arr[(index)], "column1");
   }
   for (let index = 0; index < 80; index++) {
     arr[0] = par1;
     arr[1] = par2;
-    arr[(index + 2)] = arr[index] - (arr[(index + 1)] * 1);
+    arr[(index + 2)] = arr[index] - arr[(index + 1)];
     print(arr[(index)], "column2");
   }
   for (let index = 0; index < 80; index++) {
     arr[0] = par1;
     arr[1] = par2;
-    arr[(index + 2)] = arr[index] *3 - (arr[(index + 1)] * 2);
+    arr[(index + 2)] = arr[index] * 3 - (arr[(index + 1)] * 2);
     print(arr[(index)], "column3");
+  }
+  for (let index = 0; index < 80; index++) {
+    arr[0] = par1;
+    arr[1] = par2;
+    arr[(index + 2)] = arr[index] * 4 - arr[(index + 1)];
+    print(arr[(index)], "column4");
+  }
+  for (let index = 0; index < 80; index++) {
+    arr[0] = par1;
+    arr[1] = par2;
+    arr[(index + 2)] = arr[index] + (arr[(index + 1)] * 5);
+    print(arr[(index)], "column5");
+  }
+  for (let index = 0; index < 80; index++) {
+    arr[0] = par1;
+    arr[1] = par2;
+    arr[(index + 2)] = arr[index] - (arr[(index + 1)] * 2);
+    print(arr[(index)], "column6");
   }
 }
 
 function sort() {
-  document.querySelector(".grid").innerHTML = null;
+  var array = document.querySelectorAll(".grid__cell");
+  for (let index = 0; index < array.length; index++) {
+    array[index].remove();
+  }
   var arr = document.querySelectorAll(".form__input");
   var a = arr[0].value;
   var b = arr[1].value;
@@ -51,7 +71,10 @@ function sort() {
 }
 function clear() {
   document.querySelector(".content__text").innerHTML = null;
-  document.querySelector(".grid").innerHTML = null;
+  var array = document.querySelectorAll(".grid__cell");
+  for (let index = 0; index < array.length; index++) {
+    array[index].remove();
+  }
 }
 document.querySelector(".reset").addEventListener("click", clear);
 document.querySelector(".submit").addEventListener("click", sort);
